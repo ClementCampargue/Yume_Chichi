e6e6ee86c308d9e327f1b5214aaee9ec57bb59dd
Clément <94358531+ClementCampargue@users.noreply.github.com>
2025-09-02 11:27:59 +0200

    effets supplémentaires

--- Assets/Scripts/Tools/SC_text_effect_master.cs
+++ Assets/Scripts/Tools/SC_text_effect_master.cs
@@ -1,21 +1,39 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 
 public class SC_text_effect_master : MonoBehaviour
 {
-    public TMP_Text textMeshPro;
-    [HideInInspector]public List<int> targetLetters = new List<int>();
+    [HideInInspector] public TMP_Text textMeshPro;
+    [HideInInspector] public List<int> targetLetters = new List<int>();
+    [HideInInspector] public bool coroutineStarted = false;
+
+    [Header("Noise_movement")]
+    public bool movement;
     public float amplitude = 5f;
     public float frequency = 5f;
 
+    [Header("Ondulation")]
+    public bool ondulation;
+    public float ondulation_amplitude = 5f;
+    public float ondulation_frequency = 5f;
+
+    [Header("Scale")]
+    public bool Scale;
+    public float size = 5f;
+    public float frequency_ = 5f;
+
+    [Header("Color")]
+    public bool Color_;
+    public Color color;
+
     private TMP_TextInfo textInfo;
     private Vector3[][] originalVertices;
+
+    [Header("Char toggles")]
     public string character_effect_start = "<";
     public string character_effect_end = "<";
-    [HideInInspector] public bool coroutineStarted = false;
     void Start()
     {
     }
@@ -34,68 +52,210 @@ public class SC_text_effect_master : MonoBehaviour
 
         while (true)
         {
-            originalVertices = new Vector3[textInfo.meshInfo.Length][];
-            for (int i = 0; i < textInfo.meshInfo.Length; i++)
-            {
-                originalVertices[i] = new Vector3[textInfo.meshInfo[i].vertices.Length];
-            }
-
-            textMeshPro.ForceMeshUpdate();
-            textInfo = textMeshPro.textInfo;
-
-            if (originalVertices.Length != textInfo.meshInfo.Length)
+            if (movement)
             {
                 originalVertices = new Vector3[textInfo.meshInfo.Length][];
                 for (int i = 0; i < textInfo.meshInfo.Length; i++)
                 {
                     originalVertices[i] = new Vector3[textInfo.meshInfo[i].vertices.Length];
                 }
-            }
 
-            for (int i = 0; i < textInfo.characterCount; i++)
-            {
-                TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
-                if (!charInfo.isVisible)
-                    continue;
+                textMeshPro.ForceMeshUpdate();
+                textInfo = textMeshPro.textInfo;
 
-                int c = charInfo.index;
-
-                if (targetLetters.Contains(c))
+                if (originalVertices.Length != textInfo.meshInfo.Length)
                 {
-                    int materialIndex = charInfo.materialReferenceIndex;
-                    int vertexIndex = charInfo.vertexIndex;
-                    Vector3[] vertices = textInfo.meshInfo[materialIndex].vertices;
-
-                    // Sauvegarde dynamique des positions originales
-                    if (originalVertices[materialIndex].Length < vertices.Length)
+                    originalVertices = new Vector3[textInfo.meshInfo.Length][];
+                    for (int i = 0; i < textInfo.meshInfo.Length; i++)
                     {
-                        originalVertices[materialIndex] = new Vector3[vertices.Length];
+                        originalVertices[i] = new Vector3[textInfo.meshInfo[i].vertices.Length];
                     }
-
-                    originalVertices[materialIndex][vertexIndex + 0] = vertices[vertexIndex + 0];
-                    originalVertices[materialIndex][vertexIndex + 1] = vertices[vertexIndex + 1];
-                    originalVertices[materialIndex][vertexIndex + 2] = vertices[vertexIndex + 2];
-                    originalVertices[materialIndex][vertexIndex + 3] = vertices[vertexIndex + 3];
-
-                    Vector3 offset = new Vector3(
-                        Mathf.Sin(Time.time * frequency + i) * amplitude,
-                        Mathf.Cos(Time.time * frequency + i) * amplitude,
-                        0);
-
-                    vertices[vertexIndex + 0] = originalVertices[materialIndex][vertexIndex + 0] + offset;
-                    vertices[vertexIndex + 1] = originalVertices[materialIndex][vertexIndex + 1] + offset;
-                    vertices[vertexIndex + 2] = originalVertices[materialIndex][vertexIndex + 2] + offset;
-                    vertices[vertexIndex + 3] = originalVertices[materialIndex][vertexIndex + 3] + offset;
                 }
-            }
 
-            for (int i = 0; i < textInfo.meshInfo.Length; i++)
+                for (int i = 0; i < textInfo.characterCount; i++)
+                {
+                    TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
+                    if (!charInfo.isVisible)
+                        continue;
+
+                    int c = charInfo.index;
+
+                    if (targetLetters.Contains(c))
+                    {
+                        int materialIndex = charInfo.materialReferenceIndex;
+                        int vertexIndex = charInfo.vertexIndex;
+                        Vector3[] vertices = textInfo.meshInfo[materialIndex].vertices;
+
+                        // Sauvegarde dynamique des positions originales
+                        if (originalVertices[materialIndex].Length < vertices.Length)
+                        {
+                            originalVertices[materialIndex] = new Vector3[vertices.Length];
+                        }
+
+                        originalVertices[materialIndex][vertexIndex + 0] = vertices[vertexIndex + 0];
+                        originalVertices[materialIndex][vertexIndex + 1] = vertices[vertexIndex + 1];
+                        originalVertices[materialIndex][vertexIndex + 2] = vertices[vertexIndex + 2];
+                        originalVertices[materialIndex][vertexIndex + 3] = vertices[vertexIndex + 3];
+
+                        Vector3 offset = new Vector3(
+                            Mathf.Sin(Time.time * frequency + i) * amplitude,
+                            Mathf.Cos(Time.time * frequency + i) * amplitude,
+                            0);
+
+                        vertices[vertexIndex + 0] = originalVertices[materialIndex][vertexIndex + 0] + offset;
+                        vertices[vertexIndex + 1] = originalVertices[materialIndex][vertexIndex + 1] + offset;
+                        vertices[vertexIndex + 2] = originalVertices[materialIndex][vertexIndex + 2] + offset;
+                        vertices[vertexIndex + 3] = originalVertices[materialIndex][vertexIndex + 3] + offset;
+                    }
+                }
+
+                for (int i = 0; i < textInfo.meshInfo.Length; i++)
+                {
+                    textInfo.meshInfo[i].mesh.vertices = textInfo.meshInfo[i].vertices;
+                    textMeshPro.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
+                }
+
+                yield return null;
+            }
+            if(Scale)
             {
-                textInfo.meshInfo[i].mesh.vertices = textInfo.meshInfo[i].vertices;
-                textMeshPro.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
-            }
+                textMeshPro.ForceMeshUpdate();
+                textInfo = textMeshPro.textInfo;
 
-            yield return null;
+                for (int i = 0; i < textInfo.characterCount; i++)
+                {
+                    TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
+                    if (!charInfo.isVisible)
+                        continue;
+
+                    int c = charInfo.index;
+
+                    if (targetLetters.Contains(c))
+                    {
+                        int materialIndex = charInfo.materialReferenceIndex;
+                        int vertexIndex = charInfo.vertexIndex;
+                        Vector3[] vertices = textInfo.meshInfo[materialIndex].vertices;
+
+                        Vector3 charMid = (vertices[vertexIndex + 0] + vertices[vertexIndex + 2]) / 2f;
+
+                        float scaleFactor = 1f + Mathf.Sin(Time.time * frequency_ + i) * size;
+
+                        for (int j = 0; j < 4; j++)
+                        {
+                            vertices[vertexIndex + j] = charMid + (vertices[vertexIndex + j] - charMid) * scaleFactor;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < textInfo.meshInfo.Length; i++)
+                {
+                    textInfo.meshInfo[i].mesh.vertices = textInfo.meshInfo[i].vertices;
+                    textMeshPro.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
+                }
+
+                yield return null;
+            }
+            if(ondulation)
+            {
+                originalVertices = new Vector3[textInfo.meshInfo.Length][];
+                for (int i = 0; i < textInfo.meshInfo.Length; i++)
+                {
+                    originalVertices[i] = new Vector3[textInfo.meshInfo[i].vertices.Length];
+                }
+
+                textMeshPro.ForceMeshUpdate();
+                textInfo = textMeshPro.textInfo;
+
+                if (originalVertices.Length != textInfo.meshInfo.Length)
+                {
+                    originalVertices = new Vector3[textInfo.meshInfo.Length][];
+                    for (int i = 0; i < textInfo.meshInfo.Length; i++)
+                    {
+                        originalVertices[i] = new Vector3[textInfo.meshInfo[i].vertices.Length];
+                    }
+                }
+
+                for (int i = 0; i < textInfo.characterCount; i++)
+                {
+                    TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
+                    if (!charInfo.isVisible)
+                        continue;
+
+                    int c = charInfo.index;
+
+                    if (targetLetters.Contains(c))
+                    {
+                        int materialIndex = charInfo.materialReferenceIndex;
+                        int vertexIndex = charInfo.vertexIndex;
+                        Vector3[] vertices = textInfo.meshInfo[materialIndex].vertices;
+
+                        // Sauvegarde dynamique des positions originales
+                        if (originalVertices[materialIndex].Length < vertices.Length)
+                        {
+                            originalVertices[materialIndex] = new Vector3[vertices.Length];
+                        }
+
+                        originalVertices[materialIndex][vertexIndex + 0] = vertices[vertexIndex + 0];
+                        originalVertices[materialIndex][vertexIndex + 1] = vertices[vertexIndex + 1];
+                        originalVertices[materialIndex][vertexIndex + 2] = vertices[vertexIndex + 2];
+                        originalVertices[materialIndex][vertexIndex + 3] = vertices[vertexIndex + 3];
+
+                        Vector3 offset = new Vector3(
+                            0,
+                            Mathf.Sin(Time.time * ondulation_frequency + i) * ondulation_amplitude,
+                            0);
+
+                        vertices[vertexIndex + 0] = originalVertices[materialIndex][vertexIndex + 0] + offset;
+                        vertices[vertexIndex + 1] = originalVertices[materialIndex][vertexIndex + 1] + offset;
+                        vertices[vertexIndex + 2] = originalVertices[materialIndex][vertexIndex + 2] + offset;
+                        vertices[vertexIndex + 3] = originalVertices[materialIndex][vertexIndex + 3] + offset;
+                    }
+                }
+
+                for (int i = 0; i < textInfo.meshInfo.Length; i++)
+                {
+                    textInfo.meshInfo[i].mesh.vertices = textInfo.meshInfo[i].vertices;
+                    textMeshPro.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
+                }
+
+                yield return null;
+            }
+            if (Color_)
+            {
+                textMeshPro.ForceMeshUpdate();
+                textInfo = textMeshPro.textInfo;
+
+                for (int i = 0; i < textInfo.characterCount; i++)
+                {
+                    TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
+                    if (!charInfo.isVisible)
+                        continue;
+
+                    int c = charInfo.index;
+
+                    if (targetLetters.Contains(c))
+                    {
+                        int materialIndex = charInfo.materialReferenceIndex;
+                        int vertexIndex = charInfo.vertexIndex;
+
+                        Color32[] colors = textInfo.meshInfo[materialIndex].colors32;
+
+                        colors[vertexIndex + 0] = color;
+                        colors[vertexIndex + 1] = color;
+                        colors[vertexIndex + 2] = color;
+                        colors[vertexIndex + 3] = color;
+
+                     }
+                }
+
+                for (int i = 0; i < textInfo.meshInfo.Length; i++)
+                {
+                    textInfo.meshInfo[i].mesh.colors32 = textInfo.meshInfo[i].colors32;
+                    textMeshPro.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
+                }
+
+                yield return null;
+            }
 
         }
     }
