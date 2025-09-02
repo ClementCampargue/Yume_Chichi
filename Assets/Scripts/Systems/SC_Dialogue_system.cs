1c6392b988a9df069de0b121b004ca1a69f9e863
Clément <94358531+ClementCampargue@users.noreply.github.com>
2025-09-02 09:45:45 +0200

    surprise en gros

--- Assets/Scripts/Systems/SC_Dialogue_system.cs
+++ Assets/Scripts/Systems/SC_Dialogue_system.cs
@@ -1,6 +1,8 @@
+using NUnit.Framework.Internal;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using TMPro;
 using UnityEngine;
@@ -9,11 +11,11 @@ using static UnityEngine.Rendering.DebugUI;
 
 public class SC_Dialogue_system : MonoBehaviour
 {
-     public int choice_index;
+    public int choice_index;
     public bool choice;
-    [HideInInspector]  public SC_NPC npc;
-    [HideInInspector]  public Animator npc_anim;
-    [HideInInspector]  public SC_interactive_object interactive;
+    [HideInInspector] public SC_NPC npc;
+    [HideInInspector] public Animator npc_anim;
+    [HideInInspector] public SC_interactive_object interactive;
     [HideInInspector] public TextMeshProUGUI text_component;
     [HideInInspector] public TextMeshProUGUI character_component;
     [HideInInspector] public Transform camtarget;
@@ -27,13 +29,13 @@ public class SC_Dialogue_system : MonoBehaviour
     public float speed_pause__;
     public float speed_pause___;
 
-    private int index;
+    public int index;
     [HideInInspector] public SC_Player_controller player;
     [HideInInspector] public Animator anim;
 
     [HideInInspector] public Animator cursor;
 
-    private string check; 
+    private string check;
     private Image prt_spr;
 
     private AudioSource talk_sfx;
@@ -47,6 +49,9 @@ public class SC_Dialogue_system : MonoBehaviour
 
     private bool last_dialoguie;
     private bool end;
+
+    public SC_effect_test test;
+    private bool effect_on;
 
     void Start()
     {
@@ -63,10 +68,14 @@ public class SC_Dialogue_system : MonoBehaviour
 
     void Update()
     {
-        string text2 = lines[index].Replace("%","").Replace("£", "").Replace("µ", "");
-        string text = text_component.text;
-        text = text_component.text.Replace("\n", "").Replace("\r", "").Replace(" ", "");
-        check = check.Replace("\n", "").Replace("\r", "").Replace(" ", "");
+        if(text_component.text == string.Empty)
+        {
+            test.reset_();
+        }
+
+        string text2 = lines[index].Replace("%", "").Replace("£", "").Replace("µ", "").Replace(test.character_effect_start, "").Replace(test.character_effect_end, "");
+        string text = text_component.text.Replace("\n", "").Replace("\r", "").Replace(" ", "").Replace(test.character_effect_start, "").Replace(test.character_effect_end, "");
+        check = check.Replace("\n", "").Replace("\r", "").Replace(" ", "").Replace(test.character_effect_start, "").Replace(test.character_effect_end, "");
 
 
         if (Input.GetButtonDown("Fire1"))
@@ -93,9 +102,12 @@ public class SC_Dialogue_system : MonoBehaviour
                                 anim.SetTrigger("Next_nocharacter");
                             }
                         }
+                        test.reset_();
+
                     }
                     else
                     {
+
                         if (portraits[index].name != string.Empty)
                         {
                             anim.SetTrigger("Next");
@@ -104,35 +116,6 @@ public class SC_Dialogue_system : MonoBehaviour
                         {
                             anim.SetTrigger("Next_nocharacter");
                         }
-                    }
-                    cursor.SetBool("On", false);
-
-                }
-                else if(typing)
-                {
-                    if (cursor.enabled)
-                    {
-                        cursor.SetBool("On", true);
-                    }
-                    else
-                    {
-                        cursor.SetBool("On", true);
-                        cursor.enabled = true;
-                    }
-
-                    StopAllCoroutines();
-                    text_component.text = text2;
-                    Invoke("delay_input", 0.2f);
-                }
-            }
-            else if(!end)
-            {
-                if (text == check)
-                {
-                    end = true;
-                    if (!choice)
-                    {
-                        End_dialogue();
                     }
                     cursor.SetBool("On", false);
 
@@ -148,16 +131,52 @@ public class SC_Dialogue_system : MonoBehaviour
                         cursor.SetBool("On", true);
                         cursor.enabled = true;
                     }
+
+                    StopAllCoroutines();
+                    text_component.text = text2;
+                    check_effect();
+
+                    Invoke("delay_input", 0.2f);
+                }
+            }
+            else if (!end)
+            {
+                if (text == check)
+                {
+
+                    end = true;
+                    if (!choice)
+                    {
+                        End_dialogue();
+                    }
+                    cursor.SetBool("On", false);
+                    test.reset_();
+
+                }
+                else if (typing)
+                {
+                    if (cursor.enabled)
+                    {
+                        cursor.SetBool("On", true);
+                    }
+                    else
+                    {
+                        cursor.SetBool("On", true);
+                        cursor.enabled = true;
+                    }
+                    check_effect();
+
                     StopAllCoroutines();
                     text_component.text = text2;
                     Invoke("delay_input", 0.2f);
+
                 }
             }
         }
 
         if (text == check)
         {
-  
+
             if (choice && index == lines.Length - 1)
             {
                 choices_obj.SetActive(true);
@@ -181,7 +200,7 @@ public class SC_Dialogue_system : MonoBehaviour
     {
         typing = true;
 
-        if (index < lines.Length )
+        if (index < lines.Length)
         {
             text_component.text = string.Empty;
             StartCoroutine(typeline());
@@ -199,12 +218,13 @@ public class SC_Dialogue_system : MonoBehaviour
         {
             npc_anim.Play(anims[index].name);
         }
-        check =lines[index];
+        check = lines[index];
         check = check.Replace("%", string.Empty);
         check = check.Replace("µ", string.Empty);
         check = check.Replace("£", string.Empty);
+        check = check.Replace(test.character_effect_start, "");
 
-        text_component.text = string.Empty ;
+        text_component.text = string.Empty;
         string[] words = lines[index].Split(' ');
         foreach (string word in words)
         {
@@ -241,14 +261,43 @@ public class SC_Dialogue_system : MonoBehaviour
                     text_component.text = text_component.text.Substring(0, text_component.text.Length - 1);
                     yield return new WaitForSeconds(speed_pause___);
                 }
+                else if (text_component.text.EndsWith(test.character_effect_start))
+                {
+                    effect_on =true;
+
+                    text_component.text = text_component.text.Substring(0, text_component.text.Length - 1);
+                    yield return new WaitForSeconds(speed_pause___);
+                }
+                else if (text_component.text.EndsWith(test.character_effect_end))
+                {
+                    effect_on =false;
+
+                    text_component.text = text_component.text.Substring(0, text_component.text.Length - 1);
+                    yield return new WaitForSeconds(speed_pause___);
+                }
+
+
                 else
                 {
+
                     if (!talk_sfx.isPlaying)
                     {
                         talk_sfx.Play();
                     }
-                    yield return new WaitForSeconds(speed);
                 }
+                if (effect_on)
+                {
+                    if (c.ToString() != "")
+                    {
+                        int charIndex = text_component.text.Length;
+
+                        test.targetLetters.Add(charIndex);
+
+                        test.RefreshText();
+                    }
+                }
+                yield return new WaitForSeconds(speed);
+
             }
 
         }
@@ -296,7 +345,7 @@ public class SC_Dialogue_system : MonoBehaviour
         }
 
         if (anims[index] != null)
-        { 
+        {
             npc_anim.Play(anims[index].name);
         }
         if (portraits.Count > 0)
@@ -357,7 +406,7 @@ public class SC_Dialogue_system : MonoBehaviour
         {
             anim.ResetTrigger("no_character_start");
             player.can_act = true;
-            if (portraits[index].ToString() == "null") 
+            if (portraits[index].ToString() == "null")
             {
                 anim.SetTrigger("disable_no_character");
             }
@@ -370,11 +419,11 @@ public class SC_Dialogue_system : MonoBehaviour
 
     public void Destroy_dialogue()
     {
-        if(npc != null)
+        if (npc != null)
         {
             npc.Reset_NPC();
         }
-        if(interactive != null)
+        if (interactive != null)
         {
             interactive.Reset();
         }
@@ -417,5 +466,45 @@ public class SC_Dialogue_system : MonoBehaviour
         talk_sfx.clip = talk_sound[index];
         character_component.text = character_names[index];
         camtarget.position = transform.position;
+    }
+
+
+
+    public void check_effect()
+    {
+         string testt = lines[index];
+        effect_on = false;
+        for (int i = 0; i < text_component.text.Length; i++)
+        {
+            if (testt[i+1].ToString() == test.character_effect_start)
+            {
+                effect_on = true;
+                testt = testt.Remove(i+1,1);
+
+            }
+            else if (testt[i].ToString() == test.character_effect_end)
+            {
+                effect_on = false;
+                testt = testt.Remove(i, 1);
+
+            }
+            else
+            {
+                Debug.Log(testt);
+                if (effect_on)
+                {
+                    if (testt[i].ToString() != "")
+                    {
+
+                        int charIndex = i;
+
+                        test.targetLetters.Add(charIndex);
+
+                        test.RefreshText();
+                    }
+                }
+            }
+
+        }
     }
 }
---
+++ Assets/Scripts/Tools/SC_effect_test.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine;
+
+public class SC_effect_test : MonoBehaviour
+{
+    public TMP_Text textMeshPro;
+    [HideInInspector]public List<int> targetLetters = new List<int>();
+    public float amplitude = 5f;
+    public float frequency = 5f;
+
+    private TMP_TextInfo textInfo;
+    private Vector3[][] originalVertices;
+    public string character_effect_start = "<";
+    public string character_effect_end = "<";
+    [HideInInspector] public bool coroutineStarted = false;
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (!coroutineStarted)
+        {
+            StartCoroutine(AnimateLetters());
+            coroutineStarted = true;
+        }
+    }
+
+    IEnumerator AnimateLetters()
+    {
+
+        while (true)
+        {
+            originalVertices = new Vector3[textInfo.meshInfo.Length][];
+            for (int i = 0; i < textInfo.meshInfo.Length; i++)
+            {
+                originalVertices[i] = new Vector3[textInfo.meshInfo[i].vertices.Length];
+            }
+
+            textMeshPro.ForceMeshUpdate();
+            textInfo = textMeshPro.textInfo;
+
+            if (originalVertices.Length != textInfo.meshInfo.Length)
+            {
+                originalVertices = new Vector3[textInfo.meshInfo.Length][];
+                for (int i = 0; i < textInfo.meshInfo.Length; i++)
+                {
+                    originalVertices[i] = new Vector3[textInfo.meshInfo[i].vertices.Length];
+                }
+            }
+
+            for (int i = 0; i < textInfo.characterCount; i++)
+            {
+                TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
+                if (!charInfo.isVisible)
+                    continue;
+
+                int c = charInfo.index;
+
+                if (targetLetters.Contains(c))
+                {
+                    int materialIndex = charInfo.materialReferenceIndex;
+                    int vertexIndex = charInfo.vertexIndex;
+                    Vector3[] vertices = textInfo.meshInfo[materialIndex].vertices;
+
+                    // Sauvegarde dynamique des positions originales
+                    if (originalVertices[materialIndex].Length < vertices.Length)
+                    {
+                        originalVertices[materialIndex] = new Vector3[vertices.Length];
+                    }
+
+                    originalVertices[materialIndex][vertexIndex + 0] = vertices[vertexIndex + 0];
+                    originalVertices[materialIndex][vertexIndex + 1] = vertices[vertexIndex + 1];
+                    originalVertices[materialIndex][vertexIndex + 2] = vertices[vertexIndex + 2];
+                    originalVertices[materialIndex][vertexIndex + 3] = vertices[vertexIndex + 3];
+
+                    Vector3 offset = new Vector3(
+                        Mathf.Sin(Time.time * frequency + i) * amplitude,
+                        Mathf.Cos(Time.time * frequency + i) * amplitude,
+                        0);
+
+                    vertices[vertexIndex + 0] = originalVertices[materialIndex][vertexIndex + 0] + offset;
+                    vertices[vertexIndex + 1] = originalVertices[materialIndex][vertexIndex + 1] + offset;
+                    vertices[vertexIndex + 2] = originalVertices[materialIndex][vertexIndex + 2] + offset;
+                    vertices[vertexIndex + 3] = originalVertices[materialIndex][vertexIndex + 3] + offset;
+                }
+            }
+
+            for (int i = 0; i < textInfo.meshInfo.Length; i++)
+            {
+                textInfo.meshInfo[i].mesh.vertices = textInfo.meshInfo[i].vertices;
+                textMeshPro.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
+            }
+
+            yield return null;
+
+        }
+    }
+    public void RefreshText()
+    {
+        coroutineStarted = false;
+        textMeshPro.ForceMeshUpdate();
+        textInfo = textMeshPro.textInfo;
+    }
+    public void reset_()
+    {
+        targetLetters.Clear();
+        RefreshText();
+    }
+}
