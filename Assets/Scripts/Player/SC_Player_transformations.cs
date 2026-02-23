d9e49af836295fbdf1ee11bba830c8a36d86fdd5
Clément <94358531+ClementCampargue@users.noreply.github.com>
2026-02-23 15:38:27 +0100

    debug transfos

--- Assets/Scripts/Player/SC_Player_transformations.cs
+++ Assets/Scripts/Player/SC_Player_transformations.cs
@@ -7,15 +7,26 @@ public class SC_Player_transformations : MonoBehaviour
     public SC_Player_controller controller;
     public List<GameObject> transformations;
 
+    private int currentIndex = 0;
 
     public void transform_(int index)
     {
-        foreach (GameObject gb in transformations) 
-        { 
+        if (transformations == null || transformations.Count == 0)
+            return;
+
+        if (index < 0 || index >= transformations.Count)
+            return;
+
+        foreach (GameObject gb in transformations)
+        {
             gb.SetActive(false);
         }
+
         transformations[index].SetActive(true);
         controller.animator = transformations[index].GetComponent<Animator>();
+        currentIndex = index;
+
+        Debug.Log("Transformation active : " + transformations[index].name);
     }
 
 
@@ -24,9 +35,29 @@ public class SC_Player_transformations : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (transformations == null || transformations.Count == 0)
+            return;
+
+        // Transformation précédente
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            int newIndex = currentIndex - 1;
+            if (newIndex < 0)
+                newIndex = transformations.Count - 1;
+
+            transform_(newIndex);
+        }
+
+        // Transformation suivante
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            int newIndex = currentIndex + 1;
+            if (newIndex >= transformations.Count)
+                newIndex = 0;
+
+            transform_(newIndex);
+        }
     }
 }
