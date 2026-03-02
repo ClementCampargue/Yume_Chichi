67cccc744c73c55b1d3a7676768de8f799d48a37
Clément <94358531+ClementCampargue@users.noreply.github.com>
2026-03-02 16:27:24 +0100

    fix follow

--- Assets/Scripts/Tools/SC_Player_follow.cs
+++ Assets/Scripts/Tools/SC_Player_follow.cs
@@ -7,17 +7,15 @@ public class SC_Player_follow : MonoBehaviour
     public bool Horizontal_follow = true;
     public bool Vertical_follow = true;
 
-    private Rigidbody2D rb;
 
     void Start()
     {
         target = GameObject.FindWithTag("Player").transform;
-        rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        Vector2 newPosition = rb.position;
+        Vector2 newPosition = transform.position;
 
         if (Horizontal_follow)
         {
@@ -29,6 +27,6 @@ public class SC_Player_follow : MonoBehaviour
             newPosition.y = target.position.y + offset.y;
         }
 
-        rb.MovePosition(newPosition);
+        transform.position = newPosition;
     }
 }
